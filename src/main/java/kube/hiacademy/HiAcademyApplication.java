5b911bab7bcab2c1aa9a2a6c16e83ae0fa441907
chojunbeom <jb.cho@okestro.com>
2025-07-07 00:31:18 +0900

    init

---
+++ src/main/java/kube/hiacademy/HiAcademyApplication.java
@@ -0,0 +1,13 @@
+package kube.hiacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiAcademyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kube/hiacademy/HiAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package kube.hiacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiAcademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
